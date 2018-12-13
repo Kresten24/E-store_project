55a59dd6907bb2a6f055a1308a40895b9dbbdc92
Kresten24 <ronie-8@hotmail.com>
2018-12-13 14:53:46 -0500

    Added PHP_HTML directory

--- APP/PHP_HTML/Header.php
+++ APP/PHP_HTML/Header.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<title>eStore</title>
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="Assets/CSS/home.css">
+	<link rel="stylesheet" type="text/css" href="../Assets/CSS/home.css">
 	<header>
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<a class="navbar-brand" href="#">E-store</a>
@@ -26,3 +26,4 @@
 		</nav>
 	</header>
 </head>
+<?php include('sql-connector.php'); ?>
